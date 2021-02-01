73709db7f6c7ff422e8d98068cf9db75889e3a5f
LYRbbding <35730466+LYRbbding@users.noreply.github.com>
2021-02-01 12:31:46 +0800

    new feat: 404 page

---
+++ mlib/error.js
@@ -0,0 +1,16 @@
+var timer = 5
+var interval = this.setInterval(function(){
+    if(--timer > 0){
+        $('.content .text').html('未找到当前页面<br>' + timer + '秒后返回首页')
+    }
+    else{
+        clearInterval(interval);
+        //window.location.href = "/m";
+    }
+},1000)
+
+
+$('body').on("click", ".content button",
+    function () {
+        window.location.href = "/m";
+    });
